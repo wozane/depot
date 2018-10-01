028f0e8e179fe90f6b2fbbe1ca6e17184281314b
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-10-01 14:46:13 +0200

    Add length validator to product's title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true,
+                    length: { minimum: 10,
+                              too_short: "%{count} characters is too short, minimum is 10"}
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
