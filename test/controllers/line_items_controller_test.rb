0b7313a9d137c51b9c7746ca29f387d40a8b9f83
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-10-16 06:31:35 +0200

    Add line_item counter in the cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -5,17 +5,17 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     @line_item = line_items(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get line_items_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_line_item_url
     assert_response :success
   end
 
-  test "should create line_item" do
+  test 'should create line_item' do
     assert_difference('LineItem.count') do
       post line_items_url, params: { product_id: products(:ruby).id }
     end
@@ -24,22 +24,22 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'li', 'Programming Ruby 1.9'
   end
 
-  test "should show line_item" do
+  test 'should show line_item' do
     get line_item_url(@line_item)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_line_item_url(@line_item)
     assert_response :success
   end
 
-  test "should update line_item" do
+  test 'should update line_item' do
     patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
-  test "should destroy line_item" do
+  test 'should destroy line_item' do
     assert_difference('LineItem.count', -1) do
       delete line_item_url(@line_item)
     end
